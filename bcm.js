a3af9d2e431ca9005a48befd0145176bd96a70c0
Milo Akerman <akermanmilo@gmail.com>
2024-05-07 01:10:38 -0400

    Add v1 Files

---
+++ bcm.js
@@ -0,0 +1,239 @@
+import axios from 'axios';
+import chalk from 'chalk';
+import readline from "readline";
+import fs from 'fs';
+
+const rl = readline.createInterface({ 
+    input: process.stdin,
+    output: process.stdout
+});
+
+// STARTUP SPLASH
+console.log(
+    chalk.red("+------------------------+\n") +
+    chalk.red("|") +  chalk.italic("  BROWN COURSE MANAGER  ") + chalk.red("|\n") +
+    chalk.red("|") +  chalk.italic("  By: Milo (@onemilon)  ") + chalk.red("|\n") +
+    chalk.red("+------------------------+\n")
+);
+
+// HELPERS
+// ---------------------------------------------------------------------------------------------------
+
+function parseSemID(input) {
+    if(!isNaN(input)) return input; // input is already a semester ID
+
+    let failed = false;
+    let labelInfo = input.split(" ");
+
+    let mod;
+    if(labelInfo[0] == "Summer")      mod = 0;
+    else if(labelInfo[0] == "Fall")   mod = 10;
+    else if(labelInfo[0] == "Winter") mod = 15;
+    else if(labelInfo[0] == "Spring") mod = 20;
+    else {
+        failed = true;
+        console.log("Oops! Your input did not contain any semester keywords. Please try again.");
+        rl.question(`Enter ${chalk.cyan("semester ID")} or label: `, (semesterInput)=>{
+            return parseSemID(semesterInput);
+        });
+    }
+
+    if(!failed) {
+        console.log(`Your ${chalk.cyan("semester ID")} should be ${chalk.cyan(202000 + ((parseInt(labelInfo[1]) - 2020) * 100) + mod)}\n`);
+        return (202000 + ((parseInt(labelInfo[1]) - 2020) * 100) + mod).toString();
+    }
+}
+
+function displayHelp() {
+    console.log(
+        chalk.dim("———————————————————————————————————————————————————————————\n") +
+        "Commands available:\n" + 
+        "Help — Display this message\n" +
+        "1 — Add Course to Favorites\n" +
+        "2 — Remove Course from Favorites\n" +
+        "3 — Add Courses from File to Favorites\n" +
+        "4 — Check Favorites Availability in Semester\n" +
+        chalk.dim("———————————————————————————————————————————————————————————\n")
+    );
+}
+
+
+// TODO: improve using courses sample
+function addCourseToFavorites(data, courseInput) {
+    if(courseInput.includes(" ")) {
+        // Course might be a code?
+
+        let words = courseInput.split(" ");
+        let match = data["results"].find((course) => course["code"] == words[0] + " " + words[1]);
+
+        if(match) {
+            fs.readFile('favorites.json', function (jsonErr, jsonData) {
+                let json = JSON.parse(jsonData)
+                json["courses"].push(match);
+                fs.writeFileSync("favorites.json", JSON.stringify(json))
+                userInput();
+            });
+            console.log("Added!\n");
+        } else {
+            console.log("No match found for course code! Please try again.\n");
+            userInput();
+        }
+    } else if(!isNaN(courseInput)) {
+        // Course is a CRN
+
+        let match = data["results"].find((course) => course["crn"] == courseInput);
+
+        if(match) {
+            fs.readFile('favorites.json', function (jsonErr, jsonData) {
+                let json = JSON.parse(jsonData)
+                json["courses"].push(match);
+                fs.writeFileSync("favorites.json", JSON.stringify(json))
+                userInput();
+            });
+            console.log("Added!\n");
+        } else {
+            console.log("No match found for course CRN! Please try again.\n");
+            userInput();
+        }
+    } else {
+        console.log("Course format not recognized!\n");
+        userInput();
+    }
+}
+
+function addCoursesToFavorites(data, pathInput) {
+    fs.readFile('favorites.json', async function (jsonErr, jsonData) {
+        let json = JSON.parse(jsonData);
+
+        const courseStream = readline.createInterface({
+            input: fs.createReadStream(pathInput)
+        });
+
+        courseStream.on('line', function(courseInput) {
+            if(courseInput.includes(" ")) {
+                // Course might be a code?
+        
+                let words = courseInput.split(" ");
+                let match = data["results"].find((course) => course["code"] == words[0] + " " + words[1]);
+        
+                if(match) {
+                    json["courses"].push(match);
+                }
+            } else if(!isNaN(courseInput)) {
+                // Course is a CRN
+        
+                let match = data["results"].find((course) => course["crn"] == courseInput);
+        
+                if(match) {
+                    json["courses"].push(match);
+                }
+            }
+        });
+        courseStream.on('close', () => {
+            fs.writeFileSync("favorites.json", JSON.stringify(json));
+            userInput();
+        })
+    });
+}
+
+// COMMANDS
+// ---------------------------------------------------------------------------------------------------
+
+function userInput() {
+    rl.question(`Enter a command or ${chalk.red("\"exit\"")} to end program: `, function(command) {
+        command = command.toLowerCase();
+        if(command == "exit") {
+            rl.close();
+        } else {
+            if(command == "help") displayHelp();
+
+            // Add courses to favorites
+            else if(command == "1" || command == "3") {
+                rl.question(`Enter ${chalk.cyan("semester ID")} or label: `, (semesterInput)=>{
+                    let semID = parseSemID(semesterInput);
+
+                    const dataPayload = {
+                        other: {srcdb: semID},
+                        criteria: [{field:"is_ind_study",value:"N"},{field:"is_canc",value:"N"}]
+                    }
+                    axios.post("https://cab.brown.edu/api/?page=fose&route=search&is_ind_study=N&is_canc=N", dataPayload)
+                        .then(({data}) => {
+                            if(data["fatal"]) {
+                                console.log(`Error encountered requesting Brown's course list (is your semester correct?): "${data["fatal"]}"`);
+                                userInput();
+                            }
+                            else if(command == 1) {
+                                rl.question(`Enter ${chalk.yellow("course CRN")} or ${chalk.green("course code")}: `, (courseInput)=>{
+                                    addCourseToFavorites(data, courseInput, true);
+                                })
+                            } else {
+                                rl.question(`Enter ${chalk.yellow("path to file with line-separated courses")}: `, async (pathInput)=>{
+                                    addCoursesToFavorites(data, pathInput);
+                                })
+                            }
+                        })
+                        .catch((err) => {
+                            console.log(`Error encountered requesting Brown's course list (is your semester correct?): ${err}`);
+                            userInput();
+                        })
+                });
+
+            // Remove courses from favorites
+            } else if(command == "2") {
+
+            // Check favorites availability
+            } else if(command == "4") {
+                rl.question(`Enter ${chalk.cyan("semester ID")} or label: `, (semesterInput)=>{
+                    let semID = parseSemID(semesterInput);
+
+                    const dataPayload = {
+                        other: {srcdb: semID},
+                        criteria: [{field:"is_ind_study",value:"N"},{field:"is_canc",value:"N"}]
+                    }
+                    axios.post("https://cab.brown.edu/api/?page=fose&route=search&is_ind_study=N&is_canc=N", dataPayload)
+                        .then(({data}) => {
+                            if(data["fatal"]) {
+                                console.log(`Error encountered requesting Brown's course list (is your semester correct?): "${data["fatal"]}"`);
+                                userInput();
+                            }
+                            
+                            fs.readFile('favorites.json', async function (jsonErr, jsonData) {
+                                let json = JSON.parse(jsonData);
+                                let coursesFound = [];
+                                let coursesMissed = [];
+
+                                for(const course in json["courses"]) {
+                                    let result = data["results"].find((item) => item["crn"] == json["courses"][course]["crn"]);
+                                    if(result) coursesFound.push(json["courses"][course]);
+                                    else coursesMissed.push(json["courses"][course]);
+                                }
+
+                                console.log(`${chalk.green("Courses repeated:")}`);
+                                for(let courseFound in coursesFound) {
+                                    console.log(`+ ${coursesFound[courseFound]["code"]} (${coursesFound[courseFound]["title"]})`);
+                                }
+                                console.log(`\n\n${chalk.red("Courses not found:")}`);
+                                for(let courseMissed in coursesMissed) {
+                                    console.log(`- ${coursesMissed[courseMissed]["code"]} (${coursesMissed[courseMissed]["title"]})`);
+                                }
+                                console.log("\n");
+                                userInput();
+                            });
+                        })
+                        .catch((err) => {
+                            console.log(`Error encountered requesting Brown's course list (is your semester correct?): ${err}`);
+                            userInput();
+                        })
+                });
+            }
+            //...
+            else { 
+                console.log(`Command not recognized. Please try again, or use "help" to display help message`);
+                userInput();
+            }
+        }
+    })
+}
+
+displayHelp();
+userInput();
